72e7d0fe6f82172ea3851d723995b3c976b7ecb9
Shengzhe Yao <yaoshengzhe@gmail.com>
2014-08-02 00:38:43 -0700

    add iterative solution for add two numbers

---
+++ add_two_numbers/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null || l2 != null || carry != 0) {
+            int val = carry;
+            if (l1 != null) {
+                val += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                val += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = val / 10;
+            cur.next = new ListNode(val % 10);
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
