0200beb25f776f41771f0074600b29ca36ab4a15
Shengzhe Yao <yaoshengzhe@gmail.com>
2014-08-13 01:11:38 -0700

    refine text justification

--- text_justification/Solution.java
+++ text_justification/Solution.java
@@ -1,79 +1,42 @@
-import java.lang.StringBuilder;
-
 public class Solution {
-    public ArrayList<String> fullJustify(String[] words, int L) {
-        ArrayList<String> result = new ArrayList<String>();
-        ArrayList<String> buf = new ArrayList<String>();
-        int len = 0;
-        int i = 0;
-        while (len <= L) {
-            if (i == words.length) {
-                result.add(justifyLeft(buf, L));
-                break;
+
+    public List<String> fullJustify(String[] words, int L) {
+        List<String> result = new ArrayList<String>();
+        int wordStart = 0;
+        int curLineMinLen = words[0].length() + 1;
+        for (int i=1; i < words.length; ++i) {
+            if (curLineMinLen + words[i].length() > L) {
+                int totalSpaces = i - wordStart + L - curLineMinLen;
+                int pad = (i - wordStart) == 1 ? 1 : Math.max(1, totalSpaces / (i - wordStart - 1));
+                result.add(justify(words, wordStart, i, pad, L, totalSpaces - pad * (i - wordStart - 1)));
+                wordStart = i;
+                curLineMinLen = 0;
             }
-            if (len == 0) {
-                len += words[i].length();
-                buf.add(words[i]);
-                i++;
-            } else if ((len + words[i].length() + 1) <= L) {
-                len += (words[i].length() + 1);
-                buf.add(words[i]);
-                i++;
-            } else {
-                result.add(justify(buf, L));
-                len = 0;
-                buf.clear();
-            }
+            curLineMinLen += (words[i].length() + 1);
         }
-        
+
+        if (wordStart < words.length) {
+            result.add(justify(words, wordStart, words.length, 1, L, 0));
+        }
         return result;
     }
-    
-    private String justifyLeft(List<String> buf, int L) {
-        StringBuilder builder = new StringBuilder();
-        int len = 0;
-        for (String s : buf) {
-            builder.append(s);
-            len += s.length();
-            if (len < L) {
-                builder.append(' ');
-                len++;
+    private String justify(String[] words, int start, int end, int pad, int L, int remaining) {
+        StringBuilder buf = new StringBuilder(L);
+        for (int i=start; i < end; ++i) {
+            buf.append(words[i]);
+            if (i != end - 1) {
+                for (int j=0; j < pad; ++j) {
+                    buf.append(' ');
+                }
+                if (remaining > 0) {
+                    buf.append(' ');
+                    remaining--;
+                }
             }
         }
-        while (len < L) {
-            builder.append(' ');
-            len++;            
+        for (int i=buf.length(); i < L; ++i) {
+            buf.append(' ');
         }
-        return builder.toString();
-    }
-    
-    private String justify(List<String> buf, int L) {
-        int len = 0;
-        for (String s : buf) {
-            len += s.length();
-        }
-        StringBuilder builder = new StringBuilder();
-        builder.append(buf.get(0));
-        
-        if (buf.size() == 1) {
-            for (int i=0; i < (L - len); ++i) {
-                builder.append(' ');
-            }
-            return builder.toString();
-        }
-        
-        int numOfBlank = (L - len) / (buf.size() - 1);
-        int unevenBlank = (L - len) - numOfBlank * (buf.size() - 1);
-        for (int i=1; i < buf.size(); ++i) {
-            for (int j=0; j < numOfBlank; ++j) {
-                builder.append(' ');
-            }
-            if (unevenBlank > 0) {
-                builder.append(' ');
-                unevenBlank--;
-            }
-            builder.append(buf.get(i));
-        }
-        return builder.toString();
+        return buf.toString();
     }
 }
